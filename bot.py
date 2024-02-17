d2971adbfa7ea151b7df66587019452a0b364eb5
menisadi <menisadi@gmail.com>
2024-02-17 23:44:14 +0200

    pawn moves - ambiguous option prompt

--- bot.py
+++ bot.py
@@ -350,9 +350,14 @@ def pgn(message):
 def is_ambigious(move, cid):
     moves_in_uci = list(games[cid]["Board"].legal_moves)
     moves_in_san = [games[cid]["Board"].san(move) for move in moves_in_uci]
-    possible_intended_moves = [
-        m for m in moves_in_san if move == m[0] + m[-2:]
-    ]
+    if len(move) == 2:
+        possible_intended_moves = [
+            m for m in moves_in_san if move.lower() == m
+        ]
+    else:
+        possible_intended_moves = [
+            m for m in moves_in_san if move == m[0] + m[-2:]
+        ]
     return possible_intended_moves
 
 
